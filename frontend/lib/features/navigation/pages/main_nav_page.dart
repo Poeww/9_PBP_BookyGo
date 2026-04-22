d2ac0f618dc22dcdfd7621f85a38d185e789609c
GabrielGetaWahyu <230712407@students.uajy.ac.id>
2026-04-23 00:14:26 +0700

    fixed-navbar

--- frontend/lib/features/navigation/pages/main_nav_page.dart
+++ frontend/lib/features/navigation/pages/main_nav_page.dart
@@ -44,12 +44,11 @@ class _MainNavPageState extends State<MainNavPage> {
 
     return Expanded(
       child: InkWell(
-        borderRadius: BorderRadius.circular(18),
         onTap: () => _onItemTapped(index),
         child: Center(
           child: Icon(
             icon,
-            size: isSelected ? 31 : 29,
+            size: isSelected ? 32 : 30,
             color: isSelected ? Colors.white : Colors.white70,
           ),
         ),
@@ -68,41 +67,42 @@ class _MainNavPageState extends State<MainNavPage> {
       ),
       bottomNavigationBar: SafeArea(
         top: false,
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 0, 20, 14),
-          child: Container(
-            height: 78,
-            decoration: BoxDecoration(
-              color: primaryColor,
-              borderRadius: BorderRadius.circular(24),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.10),
-                  blurRadius: 18,
-                  offset: const Offset(0, 6),
-                ),
-              ],
+        child: Container(
+          height: 82,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: primaryColor,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(26),
+              topRight: Radius.circular(26),
             ),
-            child: Row(
-              children: [
-                _buildNavItem(
-                  index: 0,
-                  icon: Icons.home_rounded,
-                ),
-                _buildNavItem(
-                  index: 1,
-                  icon: Icons.receipt_long_rounded,
-                ),
-                _buildNavItem(
-                  index: 2,
-                  icon: Icons.favorite_rounded,
-                ),
-                _buildNavItem(
-                  index: 3,
-                  icon: Icons.person_rounded,
-                ),
-              ],
-            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withValues(alpha: 0.10),
+                blurRadius: 18,
+                offset: const Offset(0, -2),
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              _buildNavItem(
+                index: 0,
+                icon: Icons.home_rounded,
+              ),
+              _buildNavItem(
+                index: 1,
+                icon: Icons.receipt_long_rounded,
+              ),
+              _buildNavItem(
+                index: 2,
+                icon: Icons.favorite_rounded,
+              ),
+              _buildNavItem(
+                index: 3,
+                icon: Icons.person_rounded,
+              ),
+            ],
           ),
         ),
       ),
